a3ac11d6141959fc2eed530d28a04e3d025027b4
Nicolas Crouzier <nicolas.crouzier@nist.gov>
2025-01-29 15:21:42 -0500

    update to 2025

--- src/main/java/gov/nist/hit/pcd/custom/ResourceHandler.java
+++ src/main/java/gov/nist/hit/pcd/custom/ResourceHandler.java
@@ -1,26 +1,37 @@
 package gov.nist.hit.pcd.custom;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
 
 import gov.nist.hit.pcd.custom.model.MDCUnitsterm;
 import gov.nist.hit.pcd.custom.model.MDCterm;
 
 public class ResourceHandler {
 
-	private static final String VERSION = "march2021";
+	private static final String VERSION = "january2025";
 	
 	private static List<MDCterm> mdcValues;
 	private static List<MDCUnitsterm> mdcUnitsTerms;
+	
+	private static HashMap<String, List<String>> hashedMdcValues = new HashMap<String, List<String>>();
 	
 	
 	public ResourceHandler() {
 		if (mdcValues == null) {
 			mdcValues = loadMDC();
+			hashedMdcValues = loadHashedMDC();
 		}
 		
 		if (mdcUnitsTerms == null) {
@@ -30,9 +41,36 @@ public class ResourceHandler {
 		
 	}
 	
+	
+	public HashMap<String, List<String>> loadHashedMDC(){
+		
+				
+	      try {
+			CSVReader csvReader = new CSVReader(new InputStreamReader(this.getClass().getResourceAsStream("/"+VERSION+"/" + "mdccodes.csv")));
+			List<String[]> arr = csvReader.readAll();
+			HashMap<String, List<String>> tableandcols = new HashMap<String, List<String>>();
+			for(String[] array : arr) {
+			    tableandcols.compute(array[0], (key,val)->val==null ? new ArrayList<>() : val).add(array[1]);
+			}
+			return tableandcols;
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (CsvException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+		
+		
+	}
+	
 	public List<MDCterm> loadMDC(){
 		
-		List<MDCterm> values = new ArrayList<>();
+		List<MDCterm> values = new ArrayList<>();				
 		try(BufferedReader br= new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("/"+VERSION+"/" + "mdccodes.csv")))) {
 		    values = br.lines().map(line -> {return new MDCterm(line.split(",")[0],line.split(",")[1]);}).collect(Collectors.toList());
 		} catch (IOException e) {
@@ -59,26 +97,38 @@ public class ResourceHandler {
 	
 	
 	public boolean codeExists(String code) {
-		for (MDCterm mdc : mdcValues) {
-			if (mdc.getTermcode().equals(code)) {
-				return true;
-			}
-		}
-		return false;
+		
+		return hashedMdcValues.containsKey(code);
+		
+//		for (MDCterm mdc : mdcValues) {
+//			if (mdc.getTermcode().equals(code)) {
+//				return true;
+//			}
+//		}
+//		return false;
 	}
 
 	
 	public boolean dupletIsValid (String code, String refid) {
-		for (MDCterm mdc : mdcValues) {
-			if (mdc.getRefid().equals(refid)) {
-				if (mdc.getTermcode().equals(code)) {
-					return true;
-				}else {
-					return false;
-				}
+		
+		List<String> values = hashedMdcValues.get(code);
+		for (String val : values) {
+			if (val.equalsIgnoreCase(refid)) {
+				return true;
 			}
 		}
 		return false;
+		
+//		for (MDCterm mdc : mdcValues) {
+//			if (mdc.getRefid().equals(refid)) {
+//				if (mdc.getTermcode().equals(code)) {
+//					return true;
+//				}else {
+//					return false;
+//				}
+//			}
+//		}
+//		return false;
 	}
 	
 	
